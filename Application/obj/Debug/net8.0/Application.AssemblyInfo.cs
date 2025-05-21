41568f53728dd1d43aaff5fdcb08dd066d185611
Guilherme Sousa <guilherme.sousa@fpf.org.br>
2025-05-21 09:15:53 -0300

    feat: Ajuste pullrequest

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e911f93dd089823e2882778d0ab0e7d24c6b9a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3166cb0fd322e4698f89dfbfb28580082dd70244")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
